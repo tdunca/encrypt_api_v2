61ecd1e7743dd0bd7234f177baf26de7bf6ecd0a
tdunca <dunca.theresa@gmail.com>
2026-02-05 15:51:07 +0100

    Added test

---
+++ tests/Api.Tests/Test1.cs
@@ -0,0 +1,27 @@
+﻿using Core;
+using Xunit;
+
+namespace Api.Tests;
+
+public class CaesarEncryptorTests
+{
+    private readonly Encryptor _encryptor= new CaesarEncryptor();
+
+    [Fact]
+    public void Encrypt_Then_Decrypt_Returns_Original()
+    {
+        var text = "Hello World!";
+        var shift = 3;
+
+        var encrypted = _encryptor.Encrypt(text, shift);
+        var decrypted = _encryptor.Decrypt(encrypted, shift);
+
+        Assert.Equal(text, decrypted);
+    }
+
+    [Fact]
+    public void Encrypt_Hello_Shift3_Is_Khoor()
+    {
+        Assert.Equal("Khoor", _encryptor.Encrypt("Hello", 3));
+    }
+}
